29e1ce7e63cf8511f178745044b22b4fa16cb12e
bohecola <bohecola@outlook.com>
2024-04-05 13:13:18 +0800

    chore: integrated tailwindcss

---
+++ tailwind.config.mjs
@@ -0,0 +1,64 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { iconsPlugin, getIconCollections  } = require("@egoist/tailwindcss-icons");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: "class",
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						a: {
+							textDecoration: "none",
+							borderBottom: `1px solid ${theme("colors.sky.300")}`
+						},
+						"a:hover": {
+							borderBottomWidth: "2px"
+						},
+						code: {
+							fontWeight: "500"
+						},
+						pre: {
+							padding: theme("padding.5"),
+							borderWidth: "1px",
+							borderColor: "#ECEBE8",
+							borderRadius: theme("borderRadius.md"),
+							color: theme("colors.slate.800"),
+							backgroundColor: "#F8F8F8"
+						}
+					}
+				},
+				invert: {
+					css: {
+						a: {
+							borderBottomColor: theme("colors.sky.400")
+						},
+						pre: {
+							borderColor: "#201E1B",
+							color: theme("colors.slate.200"),
+							backgroundColor: "#121110" // "rgb(0 0 0 / 50%)"
+						}
+					}
+				}
+			}),
+			fontFamily: {
+				mono: ["DM Mono", ...defaultTheme.fontFamily.mono]
+			},
+			backgroundImage: {
+				"grid-texture": "url('@/assets/img/grid.svg')"
+			}
+		}
+	},
+	plugins: [
+		require("@tailwindcss/typography"),
+		iconsPlugin({
+			collections: getIconCollections(["mdi", "ant-design", "carbon", "material-symbols", "ri"]),
+			scale: 1.2,
+			extraProperties: {
+				"vertical-align": "text-bottom"
+			}
+		})
+	]
+}
