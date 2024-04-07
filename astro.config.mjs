198bbd2bed97c7ca7b4e01f67d147fef433736cb
bohecola <bohecola@outlook.com>
2024-04-07 18:35:41 +0800

    fix: use sharp image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
@@ -6,7 +6,4 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  image: {
-    service: squooshImageService(),
-  },
 });
