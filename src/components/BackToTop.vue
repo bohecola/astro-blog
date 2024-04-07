41d4b8c51ae995d3e3e0a607466e6483b66b3815
bohecola <bohecola@outlook.com>
2024-04-07 21:21:33 +0800

    feat: back to top

---
+++ src/components/BackToTop.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isActive = ref(false);
+
+window.addEventListener("scroll", () => {
+	isActive.value = window.scrollY > 100;
+});
+
+const backToTop = () => {
+	window.scrollTo({
+		top: 0
+	});
+};
+</script>
+
+<template>
+  <button
+    :class="`
+      block fixed right-3 bottom-3
+      w-10 h-10 text-center rounded-full
+      opacity-30 hover:opacity-100
+      transition-all duration-300
+      z-20 hover:bg-[#88888833]
+      ${isActive ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}
+    `"
+    @click="backToTop"
+  >
+    <span class="i-material-symbols-arrow-upward-rounded " />
+  </button>
+</template>
