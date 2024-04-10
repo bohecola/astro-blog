af386a7f0bd5211f043808f62140bb52163e9d74
bohecola <bohecola@outlook.com>
2024-04-10 09:01:28 +0800

    feat: generateToC

---
+++ src/utils/generateToC.ts
@@ -0,0 +1,35 @@
+import type { MarkdownHeading } from "astro";
+import { PAGE_TITLE_ID } from "../constants";
+
+export interface TocItem extends MarkdownHeading {
+  children: TocItem[];
+}
+
+interface TocOpts {
+  minHeadingLevel: number;
+  maxHeadingLevel: number;
+  title: string;
+}
+
+// 将Astro生成的平面标题数组转换为嵌套树结构
+export function generateToC(
+  headings: MarkdownHeading[],
+  { minHeadingLevel, maxHeadingLevel, title }: TocOpts
+) {
+  headings = headings.filter(({ depth }) => depth >= minHeadingLevel && depth <= maxHeadingLevel);
+  const toc: TocItem[] = [{ depth: 2, slug: PAGE_TITLE_ID, text: title, children: [] }];
+  for (const heading of headings) {
+    injectChild(toc, { ...heading, children: [] });
+  }
+  return toc;
+}
+
+// 根据深度属性，将 item 注入到树中对应的深度
+function injectChild(items: TocItem[], item: TocItem): void {
+  const lastItem = items.at(-1);
+  if (!lastItem || lastItem.depth >= item.depth) {
+    items.push(item);
+  } else {
+    return injectChild(lastItem.children, item);
+  }
+}
