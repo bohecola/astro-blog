447178fa503d14e1ea48b22948b9ed0ce3d63bc3
bohecola <bohecola@outlook.com>
2024-04-12 20:21:28 +0800

    feat: grid background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const { iconsPlugin, getIconCollections  } = require("@egoist/tailwindcss-icons");
+const svgToDataUri = require("mini-svg-data-uri");
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette')
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -59,6 +61,24 @@ export default {
 			extraProperties: {
 				"vertical-align": "text-bottom"
 			}
-		})
+		}),
+		function ({ addVariant }) {
+      addVariant(
+        'supports-backdrop-blur',
+        '@supports (backdrop-filter: blur(0)) or (-webkit-backdrop-filter: blur(0))'
+      )
+		},
+		function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'bg-grid': (value) => ({
+						backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+					}),
+				},
+				{ values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+			)
+		}
 	]
 }
