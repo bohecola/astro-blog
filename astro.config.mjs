cf2cdf87d3d87f0a6f36176572df9a42d5c5e996
bohecola <bohecola@outlook.com>
2024-04-13 22:19:02 +0800

    chore: set vitesse-black as dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     shikiConfig: {
       themes: {
         light: 'vitesse-light',
-        dark: 'vitesse-dark'
+        dark: 'vitesse-black'
       },
     }
   }
