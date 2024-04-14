4f00e25cb66e98a4661b7de677555208aed56f93
bohecola <bohecola@outlook.com>
2024-04-14 14:56:48 +0800

    chore: config rss, sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
+
+import { SITE } from './src/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), vue()],
+  site: SITE.website,
+  integrations: [
+    tailwind(),
+    mdx(),
+    vue(),
+    sitemap()
+  ],
   markdown: {
     shikiConfig: {
       themes: {
         light: 'vitesse-light',
         dark: 'vitesse-black'
-      },
+      }
     }
   }
 });
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import type { Site } from "./types";
+
+export const SITE: Site = {
+  website: "https://blog.colan.top/",
+  author: "bohecola",
+  desc: "bohecola's blog",
+  title: "bohecola",
+}
