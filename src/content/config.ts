05423dbfaca20576eb80c0d6508033518f5defbb
bohecola <bohecola@outlook.com>
2024-04-14 14:54:11 +0800

    fix: frontmatter field optional

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,14 @@ const postsCollection = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
-    pubDate: z.date(),
+    pubDate: z.date().optional(),
     description: z.string().optional(),
     author: z.string().optional(),
     image: z.object({
       url: z.string(),
       alt: z.string(),
     }).optional(),
-    tags: z.array(z.string()),
+    tags: z.array(z.string()).optional(),
     draft: z.boolean().optional(),
   })
 });
