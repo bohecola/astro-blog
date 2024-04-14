8e54a2490c2d76166ac66a02d9e896cb24fb1ae3
bohecola <bohecola@outlook.com>
2024-04-14 15:22:54 +0800

    chore: update site config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { Site } from "./types";
 
 export const SITE: Site = {
   website: "https://blog.colan.top/",
-  author: "bohecola",
   desc: "bohecola's blog",
   title: "bohecola",
+  author: "bohecola",
 }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import { SITE } from '@/config';
 
 export async function GET(context) {
   const posts = await getCollection("posts");
   return rss({
-    title: 'Bohecola\'s blog',
-    description: 'Blog content about front-end, web development, and Node.js.',
+    title: SITE.title,
+    description: SITE.desc,
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
