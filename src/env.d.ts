205f12a3af5c3c93f055c455b684b38c7734cfe6
bohecola <bohecola@outlook.com>
2024-04-16 19:07:43 +0800

    chore: config vercel env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GISCUS_USERNAME: string;
+  readonly PUBLIC_GISCUS_REPO: string;
+  readonly PUBLIC_GISCUS_REPO_ID: string;
+  readonly PUBLIC_GISCUS_CATEGORY: string;
+  readonly PUBLIC_GISCUS_CATEGORY_ID: string;
+  // 更多环境变量…
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
