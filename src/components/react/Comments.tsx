9f6bd6b8077335a0178d8681534563362ca550df
bohecola <bohecola@outlook.com>
2024-04-16 23:57:57 +0800

    feat: giscus theme change

--- src/components/react/Comments.tsx
+++ src/components/react/Comments.tsx
@@ -1,14 +1,34 @@
 import Giscus from "@giscus/react";
+import { useEffect, useState } from "react";
 
 const { 
   PUBLIC_GISCUS_USERNAME,
   PUBLIC_GISCUS_REPO,
   PUBLIC_GISCUS_REPO_ID,
-  PUBLIC_GISCUS_CATEGORY,
   PUBLIC_GISCUS_CATEGORY_ID
 } = import.meta.env;
 
+// 得到主题
+function getCurrentTheme() {
+  const storedTheme = typeof localStorage !== 'undefined' && localStorage.getItem('theme');
+  return storedTheme || (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark');
+}
+
 export default function Comments () {
+  const initialTheme = getCurrentTheme();
+
+  const themeChangeHandler = ({ detail: { themeValue } }) => {
+    setTheme(themeValue);
+  }
+  const [theme, setTheme] = useState(initialTheme);
+
+  useEffect(() => {
+    window.addEventListener("app:themechange", themeChangeHandler);
+
+    return () => {
+      window.removeEventListener("app:themechange", themeChangeHandler);
+    };
+  }, []);
 
   return (
     <div>
@@ -16,7 +36,7 @@ export default function Comments () {
         id="comments"
         repo={`${PUBLIC_GISCUS_USERNAME}/${PUBLIC_GISCUS_REPO}`}
         repoId={PUBLIC_GISCUS_REPO_ID}
-        category={PUBLIC_GISCUS_CATEGORY}
+        category="Announcements"
         categoryId={PUBLIC_GISCUS_CATEGORY_ID}
         mapping="og:title"
         reactionsEnabled="1"
@@ -24,8 +44,8 @@ export default function Comments () {
         inputPosition="top"
         lang="zh-CN"
         loading="lazy"
-        theme={'dark'}
+        theme={theme}
       />
     </div>
-  )
+  );
 }
